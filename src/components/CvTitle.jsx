39ef85a399a10c3ea0778cbd6277639c5eaa555b
raph-bard <raphael.bard.pro@googlemail.com>
2023-12-05 11:24:42 +0100

    adding description and soft skills

--- src/components/CvTitle.jsx
+++ src/components/CvTitle.jsx
@@ -3,13 +3,13 @@ export default function CvTitle() {
     <div className="cv-title">
       <h1 className="mb-10">
         Raphaël Bard <br />
-        <b className="blue-text">Développeur Web Junior - Recherche alternance</b>
+        <b className="blue-text">
+          Développeur Web Junior - Recherche alternance
+        </b>
       </h1>
-      <p className="grey-text mb-20">
-        <b>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis
-          libero nemo porro obcaecati doloribus excepturi voluptas quam, nobis
-          consequatur ipsum voluptatum dolores, atque, commodi ad.
+      <p className="description grey-text mb-20">
+        <b>      
+          Développeur Web Junior en quête d'une alternance, j'ai initialement travaillé en tant qu'intégrateur Web. Cherchant à élargir mes compétences, j'ai entrepris une formation à la Wild Code School Bordeaux, me spécialisant désormais dans le développement Web et Web Mobile. Cette transition m'a permis d'approfondir mes connaissances et de renforcer ma capacité à concrétiser des conceptions visuelles en solutions fonctionnelles.
         </b>
       </p>
       <hr className="mb-20" />
